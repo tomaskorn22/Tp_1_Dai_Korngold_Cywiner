aa7e499a1f92bce0bbc859f59c32d476b358c286
Lucas Tjor <tjor75@gmail.com>
2025-03-28 09:41:25 -0300

    Ejercicio 1 realizado

---
+++ src/ej01.js
@@ -0,0 +1,14 @@
+let textoEntrada01 = "Escuela", textoEntrada02 = "ORT";
+let textoSalida = "";
+textoSalida = concatInvert(textoEntrada01, textoEntrada02);
+console.clear(); // Borra la pantalla en la consola.
+console.log(`Textos de Entrada: "${textoEntrada01}" y "${textoEntrada02}"`);
+console.log(`Texto de Salida: "${textoSalida}"`);
+
+function concatInvert (texto1, texto2){
+    // Invertimos la cadena resultante
+    let invertido = texto1.split('').reverse().join('');
+    let invertido2= texto2.split('').reverse().join('');
+    let concatenado = invertido2 + invertido;   
+    return concatenado;
+}
